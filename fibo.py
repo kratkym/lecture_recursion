da2e025d81284ad6a64913127081abd9ea5ef9ee
Marek Kratky <272230@vutbr.cz>
2025-04-24 09:06:35 +0200

    test

--- fibo.py
+++ fibo.py
@@ -1,7 +1,15 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n > 1:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 
 def main():
-    pass
-
+    n = int(input("zadejte číslo: "))
+    vysledek = recursive_nth_fibo(n)
+    print(f"{n}. Fibonacciho číslo je: {vysledek}")
 
 if __name__ == "__main__":
     main()
